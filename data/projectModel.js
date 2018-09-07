00c4dcb2eb6808f28779c78a831eee2e46069ced
AI2EJAY <JHammett1314@gmail.com>
2018-09-07 11:06:11 -0500

    Hitting a weird error that has blocked me for about 5 minutes so far

--- data/projectModel.js
+++ data/projectModel.js
@@ -1,3 +1,21 @@
-const express = require('express')
-const PMdb = require('./helpers/projectModel') 
-const router = express.Router()
+const express = require("express");
+const PMdb = require("./helpers/projectModel");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  PMdb.get()
+    .then(pm => {
+      res.status(200).json(pm);
+    })
+    .catch(err => {
+        console.error(`There was an error in your Get function:  ===${err}===`)
+        res.status(500).json({ errMsg: "Unable to fetch Project Model Data" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params
+    console.log(id)
+    // PMdb.get(id)
+})
+module.exports = router;
--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const server = express();
 
 const projectModel = require('./data/helpers/projectModel')
-const actionModel = require('./data/helpers/actionModel')
+// const actionModel = require('./data/helpers/actionModel')
 
-server.use(express.json())
+server.use(express.json());
 server.use('/projectModel', projectModel);
-server.use('/actionModel', actionModel);
+// server.use('/actionModel', actionModel);
 
 const port = 9000;
 
