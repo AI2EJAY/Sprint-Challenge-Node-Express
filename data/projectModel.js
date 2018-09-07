7566e80d8e90a0b12620ef5aff4e9c71ab87de9c
AI2EJAY <JHammett1314@gmail.com>
2018-09-07 12:22:57 -0500

    Project Models rougly done.
    -Will make them have more strict/detailed error messages later

--- data/projectModel.js
+++ data/projectModel.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const PMdb = require('./helpers/projectModel');
+const PMdb = require("./helpers/projectModel");
 
 router.get("/", (req, res) => {
   PMdb.get()
@@ -16,15 +16,55 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   console.log(id);
-  // PMdb.get(id)
+  PMdb.get(id)
+    .then(pm => {
+      res.status(200).json(pm);
+    })
+    .catch(err => {
+      console.error(`There was an error in your Get function:  ===${err}===`);
+      res.status(500).json({ errMsg: "Unable to fetch Project Model Data" });
+    });
 });
 
 router.post("/", (req, res) => {
-  content = req.body;
+  const content = req.body;
+  PMdb.insert(content)
+    .then(pm => {
+      res.status(200).json(pm);
+    })
+    .catch(err => {
+      console.error(`There was an error in your Post function:  ===${err}===`);
+      res.status(500).json({ errMsg: "Unable to create post" });
+    });
 });
 
-router.put();
+router.put("/:id", (req, res) => {
+  const content = req.body;
+  const { id } = req.params;
+  if (id) {
+    PMdb.update(id, content)
+      .then(pm => {
+        res.status(200).json({ pm });
+      })
+      .catch(err => {
+        console.error(`There was an error in your Put function:  ===${err}===`);
+        res.status(500).json({ errMsg: "Unable to add data to target" });
+      });
+  } else return null;
+});
 
-router.delete();
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  PMdb.remove(id)
+    .then(pm => {
+      res.status(200).json({ pm });
+    })
+    .catch(err => {
+      console.error(
+        `There was an error in your Delete function:  ===${err}===`
+      );
+      res.status(500).json({ errMsg: "Unable to delete target" });
+    });
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 
-const projectModel = require('./data/helpers/projectModel')
-// const actionModel = require('./data/helpers/actionModel')
+const projectModel = require('./data/projectModel.js')
+// const actionModel = require('./data/actionModel.js')
 
 server.use(express.json());
-server.use("/projectModel", projectModel)
 server.use(helmet())
 server.use(cors())
 server.use(morgan("dev"))
+server.use("/projectModel", projectModel)
 
 const port = 9000;
 
